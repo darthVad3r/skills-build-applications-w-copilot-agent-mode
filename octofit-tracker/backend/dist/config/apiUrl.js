c52739264159d42c6d425e6dab7489d6967c4d98
Bobby Finley <rmfinley@yahoo.com>
2026-07-30 18:20:48 +0000

    refactor: restructure backend code and update server entry point

--- octofit-tracker/backend/dist/config/apiUrl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiPort = exports.apiBaseUrl = void 0;
-const apiPort = 8000;
-exports.apiPort = apiPort;
-const codespaceName = process.env.CODESPACE_NAME;
-exports.apiBaseUrl = codespaceName
-    ? `https://${codespaceName}-${apiPort}.app.github.dev`
-    : `http://localhost:${apiPort}`;
